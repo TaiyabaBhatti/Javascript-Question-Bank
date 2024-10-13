033e9cd15990e9522180d4c123d6ab2f95958e5b
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-10-14 02:40:46 +0500

    added

--- src/freecodeCamp-Javascript/platformerGame.js
+++ src/freecodeCamp-Javascript/platformerGame.js
@@ -1,1 +1,28 @@
- 
+ const startBtn=document.getElementById("start-btn");
+ const canvas=document.getElementById("canvas");  // screen 3 game screen 
+ const startScreen=document.querySelector(".start-screen");  //screen 1
+ const checkpointScreen=document.querySelector(".checkpoint-screen"); //screen 2 winning screen
+
+ const checkpointMessage = document.querySelector(".checkpoint-screen > p");  //winning script
+
+
+//  Using Canvas API for creating graphics
+
+
+const ctx = canvas.getContext("2d");  // its a drawing object for canvas
+canvas.width = innerWidth;
+canvas.height = innerHeight;       //using this format fro better internal pixel resolutions
+
+const gravity = 0.5;
+let isCheckpointCollisionDetectionActive =true;
+
+//responsive ness for every component,
+const proportionalSize = (size) => {
+return innerHeight<500?Math.ceil((size / 500) * innerHeight):size;
+}
+
+// characteristics for main player
+
+class Player{
+
+}
