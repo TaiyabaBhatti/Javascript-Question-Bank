b9ddd7c6b213fdc4a4f4f7d4bfb079cbb116e399
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-12-15 15:05:55 +0500

    modified

--- Polyrhythms/sound.js
+++ Polyrhythms/sound.js
@@ -1,21 +1,19 @@
 // using webAudio APi
 
-const audioCtx  = new (window.AudioContext)();
+const audioCtx = new window.AudioContext();
 
-function playSound(frequency=440,duration=0.5){
+function playSound(frequency = 440, duration = 0.5) {
+  const oscillatorDevice = audioCtx.createOscillator();
+  const envelop = audioCtx.createGain();
 
-const oscillatorDevice = audioCtx.createOscillator();
- const envelop = audioCtx.createGain();
+  oscillatorDevice.connect(envelop);
+  envelop.connect(audioCtx.destination);
 
-oscillatorDevice.connect(envelop)
-envelop.connect(audioCtx.destination);
+  envelop.gain.setValueAtTime(0, audioCtx.currentTime);
+  envelop.gain.linearRampToValueAtTime(0.05, audioCtx.currentTime + 0.05);
+  envelop.gain.linearRampToValueAtTime(0, audioCtx.currentTime + duration);
 
-envelop.gain.setValueAtTime(0, audioCtx.currentTime);
-envelop.gain.linearRampToValueAtTime(0.05,audioCtx.currentTime+0.05);
-envelop.gain.linearRampToValueAtTime(0,audioCtx.currentTime + duration);
-
-oscillatorDevice.frequency.setValueAtTime(frequency,audioCtx.currentTime)
-oscillatorDevice.start();
-oscillatorDevice.stop(audioCtx.currentTime + duration)
-
+  oscillatorDevice.frequency.setValueAtTime(frequency, audioCtx.currentTime);
+  oscillatorDevice.start();
+  oscillatorDevice.stop(audioCtx.currentTime + duration);
 }
--- Polyrhythms/trackClass.js
+++ Polyrhythms/trackClass.js
@@ -1,30 +1,29 @@
-class Track{
-    constructor(center, radius){
-this.center=center;
-this.radius=radius;
-this.period =Math.PI;
-    }
+class Track {
+  constructor(center, radius) {
+    this.center = center;
+    this.radius = radius;
+    this.period = Math.PI;
+  }
 
-    draw(ctx){
-ctx.beginPath();
-// ctx.arc(this.center.x,this.center.y,this.radius,0,Math.PI*2)
-for (let line = 0; line <= Math.PI * 2; line+= 0.1) {
-    let pos = this.getPosition(line)
-   ctx.lineTo(pos.x,pos.y)
-//    ctx.closePath();
-}
-ctx.closePath();
-ctx.strokeStyle = "white";
-ctx.stroke();  //do the drawing now
+  draw(ctx) {
+    ctx.beginPath();
+    // ctx.arc(this.center.x,this.center.y,this.radius,0,Math.PI*2)
+    for (let line = 0; line <= Math.PI * 2; line += 0.1) {
+      let pos = this.getPosition(line);
+      ctx.lineTo(pos.x, pos.y);
+      //    ctx.closePath();
     }
+    ctx.closePath();
+    ctx.strokeStyle = "white";
+    ctx.stroke(); //do the drawing now
+  }
 
-    getPosition(ballOffset){
-      
-return {
-    x:this.center.x + Math.cos(ballOffset) * this.radius  ,
-    y:this.center.y - Math.abs(Math.sin(ballOffset)) * this.radius,
-    round:Math.floor(ballOffset/this.period)
-}
-    }
-// returning position according to angle offset
+  getPosition(ballOffset) {
+    return {
+      x: this.center.x + Math.cos(ballOffset) * this.radius,
+      y: this.center.y - Math.abs(Math.sin(ballOffset)) * this.radius,
+      round: Math.floor(ballOffset / this.period),
+    };
+  }
+  // returning position according to angle offset
 }
